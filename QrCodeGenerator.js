89068a0d51680b38d2eb2cc3e1b2d3693fdf1896
Bhanu Pratap Singh <pbhanu314@gmail.com>
2019-06-29 16:29:21 +0530

    File Added to repository

---
+++ QrCodeGenerator.js
@@ -0,0 +1,27 @@
+QrCodeGenerator = {
+
+	qrCode: null,
+
+	generateQrCode: function(qrCodeContent) {
+		$('#qrCode').empty();
+
+		QrCodeGenerator.qrCode = new QRCode(document.getElementById('qrCode'), {
+			text: qrCodeContent,
+			width: 384 - (28 * 2),
+			height: 384 - (28 * 2),
+			colorDark: "#000000",
+			colorLight: "#ffffff",
+			correctLevel: QRCode.CorrectLevel.H,
+		});
+	},
+};
+
+$(document).ready(function() {
+	$('#qrCodeContentTextArea').on('keyup', function() {
+		QrCodeGenerator.generateQrCode($('#qrCodeContentTextArea').val());
+	});
+
+	$('#generateQrCodeButton').click(function() {
+		QrCodeGenerator.generateQrCode($('#qrCodeContentTextArea').val());
+	});
+});
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-#My Initial project
+# QR Share 
+This project is about Sharing Data with the help a web page using QrCode.
