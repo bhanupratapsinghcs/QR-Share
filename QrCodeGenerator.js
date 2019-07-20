8a44b8d76a900242285bf6afea261a32766284e3
bhanupratapsinghcs <pbhanu314@gmail.com>
2019-07-20 10:25:58 +0530

    Download Button added,css change,js script change

--- QrCodeGenerator.js
+++ QrCodeGenerator.js
@@ -1,27 +1,69 @@
 QrCodeGenerator = {
+	
 
-	qrCode: "QRShare & Generator",
+	generateQrCode: function(qrCodeContent){
+		document.getElementById('qrCode').innerHTML = "";
 
-	generateQrCode: function(qrCodeContent) {
-		$('#qrCode').empty();
-
-		QrCodeGenerator.qrCode = new QRCode(document.getElementById('qrCode'), {
-			text: qrCodeContent,
-			width: 384 - (28 * 2),
-			height: 384 - (28 * 2),
-			colorDark: "#000000",
-			colorLight: "#ffffff",
-			correctLevel: QRCode.CorrectLevel.H,
+		var qrcode = new QRCode(document.getElementById('qrCode'), {
+		    text: qrCodeContent,
+		    width: 326,
+		    height: 326,
+		    colorDark : "#ff0000",
+		    colorLight : "#fff000",
+		    correctLevel : QRCode.CorrectLevel.H,
 		});
 	},
 };
 
-$(document).ready(function() {
-	$('#qrCodeContentTextArea').on('keyup', function() {
-		QrCodeGenerator.generateQrCode($('#qrCodeContentTextArea').val());
-	});
+window.onload = function(){
 
-	$('#generateQrCodeButton').click(function() {
-		QrCodeGenerator.generateQrCode($('#qrCodeContentTextArea').val());
-	});
-});
+	document.getElementById('generateQrCodeButton').addEventListener('click',displayButton);
+
+	document.getElementById('dwnld').addEventListener('click',dwnld);
+
+	function displayButton(){
+		var txt = document.getElementById('qrCodeContentTextArea').value;
+
+		QrCodeGenerator.generateQrCode(txt);
+	};
+
+	function dwnld() {
+		var url = document.getElementById('test').lastChild.getAttribute('src');
+		var link = document.createElement('a');
+		link.download = 'rq';
+		link.href = url;
+		link.click();
+		console.log(url)
+	};
+
+};
+
+
+// QrCodeGenerator = {
+
+// 	qrCode: null,
+
+// 	generateQrCode: function(qrCodeContent) {
+// 		$('#qrCode').empty();
+
+// 		QrCodeGenerator.qrCode = new QRCode(document.getElementById('qrCode'), {
+// 			text: qrCodeContent,
+// 			width: 384 - (28 * 2),
+// 			height: 384 - (28 * 2),
+// 			colorDark: "#000000",
+// 			colorLight: "#ffffff",
+// 			correctLevel: QRCode.CorrectLevel.H,
+// 		});
+// 	},
+// };
+
+// $(document).ready(function() {
+// 	$('#qrCodeContentTextArea').on('keyup', function() {
+// 		QrCodeGenerator.generateQrCode($('#qrCodeContentTextArea').val());
+// 	});
+
+// 	$('#generateQrCodeButton').click(function() {
+// 		QrCodeGenerator.generateQrCode($('#qrCodeContentTextArea').val());
+// 	});
+
+// });
