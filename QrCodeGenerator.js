f5f0ff213bdcb1cc3a0648e32fee083693c3cc4f
bhanupratapsinghcs <pbhanu314@gmail.com>
2019-07-29 23:12:01 +0530

    new Style sheet added

--- QrCodeGenerator.js
+++ QrCodeGenerator.js
@@ -8,18 +8,14 @@ QrCodeGenerator = {
 		    text: qrCodeContent,
 		    width: 326,
 		    height: 326,
-		    colorDark : "#ff0000",
-		    colorLight : "#fff000",
+		    colorDark : "#000000",
+		    colorLight : "#ffffff",
 		    correctLevel : QRCode.CorrectLevel.H,
 		});
 	},
 };
 
 window.onload = function(){
-
-	document.getElementById('generateQrCodeButton').addEventListener('click',displayButton);
-
-	document.getElementById('dwnld').addEventListener('click',dwnld);
 
 	function displayButton(){
 		var txt = document.getElementById('qrCodeContentTextArea').value;
@@ -29,19 +25,25 @@ window.onload = function(){
 
 	function dwnld() {
 		var edge = document.getElementById("qrCode").firstChild;
-
+		//  Egde Support
 		if(window.navigator.msSaveBlob){
 			window.navigator.msSaveBlob(edge.msToBlob(), 'qrCode.png');
-		}else{
+		}
+		// Chrome and other desktop browser support
+		else{
 			// var url = document.getElementById('test').lastChild.getAttribute('src');
 			var url = edge.toDataURL();
 			var link = document.createElement('a');
 			link.download = 'QrCode';
 			link.href = url;
 			link.click();
+			// document.getElementById('sp').innerHTML= url;
 		}
 	};
 
+	document.getElementById('generateQrCodeButton').addEventListener('click',displayButton);
+
+	document.getElementById('dwnld').addEventListener('click',dwnld);
 };
 
 
